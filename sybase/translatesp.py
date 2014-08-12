fc6ee2f964dc06083145bd295d60a34dd03441cc
Lori Corbani <Lori.Corbani@jax.org>
2014-08-12 16:47:02 +0000

    TR11654/translateSP

--- sybase/translatesp.py
+++ sybase/translatesp.py
@@ -1,11 +1,12 @@
 #!/usr/local/bin/python
 
+#
+# translate the sybase stored procedure to postgres
+#
+
 import sys 
 import os
-
-#
-# Main
-#
+import string
 
 spFunc = sys.argv[1]
 sybSP = os.getenv('MGD_DBSCHEMADIR') + '/procedure/' + spFunc + '_create.object'
@@ -16,7 +17,12 @@ sybIn = open(sybSP, 'r')
 pgOut = open(pgSP, 'w+')
 
 isCreate = 0
-isCheckpoint = 0
+isGo = 0
+isDeclare = 0
+isBegin = 0
+isVariable = 0
+
+trackVars = [];
 
 for r in sybIn.readlines():
 
@@ -39,35 +45,56 @@ for r in sybIn.readlines():
 		r = ')\nRETURNS VOID AS\n\\$\\$\n'
 		isCreate = 0
 
-	elif isCreate:
-		print 'in create...'
+	elif isCreate and r.find('integer') >= 0:
+		trackVars.append('int')
+
+	elif isCreate and r.find('varchar') >= 0:
+		trackVars.append('varchar')
 
 	elif r.find('declare') >= 0:
 		r = r.replace('declare', 'DECLARE')
 		r = r + '\nBEGIN\n'
+		isDeclare = 1
+		isBegin = 1
 
-	elif r.find('go\n') >= 0 and not isCheckpoint:
+	elif (isDeclare \
+	     or r.find('select') >= 0 \
+	     or r.find('insert') >= 0 \
+	     or r.find('update') >= 0 \
+	     or r.find('delete') >= 0 \
+	     ) \
+	     and not isBegin:
+		r = 'BEGIN\n\n' + r
+		isBegin = 1
+
+	elif r.find('go\n') >= 0 and not isGo:
 		r = r.replace('go\n', ';\n')
 
-	elif r.find('go\n') >= 0 and isCheckpoint:
+	elif r.find('go\n') >= 0 and isGo:
+		continue
+
+	elif r.find('use') >= 0:
 		continue
 
 	elif r.find('checkpoint') >= 0:
-		isCheckpoint = 1
+		isGo = 1
 		continue
 
 	elif r.find('use ') >= 0 \
-		or r.find('quit') >= 0:
+	     or r.find('quit') >= 0:
 		continue
 
 	elif r.find('EOSQL') >= 0:
 		r = 'END;\n\\$\\$\nLANGUAGE plpgsql;\n\n'
-		r = r + 'GRANT EXECUTE ON FUNCTION %s(int, int) TO public;\n\n' % (spFunc)
+		r = r + 'GRANT EXECUTE ON FUNCTION %s(' % (spFunc)
+		r = r + string.join(trackVars, ',')
+		r = r + ') TO public;\n\n'
 		r = r + 'EOSQL'
 
 	r = r.replace('@', '')
-	r = r.replace('integer', 'int;')
+	r = r.replace('integer', 'int')
 	r = r.replace('user_name()', 'current_user;')
+	r = r.replace('"', '\'')
 
 	pgOut.write(r)
 
@@ -75,5 +102,4 @@ sybIn.close()
 pgOut.close()
 
 os.system('chmod 775 %s' % (pgSP))
-
 
