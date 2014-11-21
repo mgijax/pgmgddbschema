c583f99c640058d6098061cb5d2210545479be4a
MGI Admin <mgiadmin@jax.org>
2014-11-21 16:55:11 +0000

    TR11750/postgres

--- sybase/editcreatetrigger.sh
+++ sybase/editcreatetrigger.sh
@@ -7,7 +7,7 @@
 cd `dirname $0` && . ../Configuration
 
 #
-# read thru triggers_deletion.txt
+# read thru triggers_delete.txt
 #
 
 #while IFS=: read object mgikey mgitype text
@@ -18,11 +18,11 @@ echo $mgikey
 echo $mgitype
 #echo $text
 
-cp ${MGD_DBSCHEMADIR}/trigger/${object}_create.object ${POSTGRESTRIGGER}
-cp ${POSTGRESTRIGGER}/template_delete_create.object.new ${POSTGRESTRIGGER}/${object}_delete_create.object
-cp ${POSTGRESTRIGGER}/template_delete_drop.object.new ${POSTGRESTRIGGER}/${object}_delete_drop.object
+#cp ${MGD_DBSCHEMADIR}/trigger/${object}_create.object ${PG_MGD_DBSCHEMADIR}/trigger
+cp ${PG_MGD_DBSCHEMADIR}/trigger/template_delete_create.object.new ${PG_MGD_DBSCHEMADIR}/trigger/${object}_delete_create.object
+cp ${PG_MGD_DBSCHEMADIR}/trigger/template_delete_drop.object.new ${PG_MGD_DBSCHEMADIR}/trigger/${object}_delete_drop.object
 
-ed ${POSTGRESTRIGGER}/${object}_delete_create.object <<END
+ed ${PG_MGD_DBSCHEMADIR}/trigger/${object}_delete_create.object <<END
 g/PG-TABLE/s//${object}/g
 g/PG-KEY/s//${mgikey}/g
 g/PG-TYPE/s//${mgitype}/g
@@ -31,7 +31,7 @@ w
 q
 END
 
-ed ${POSTGRESTRIGGER}/${object}_delete_drop.object <<END
+ed ${PG_MGD_DBSCHEMADIR}/trigger/${object}_delete_drop.object <<END
 g/PG-TABLE/s//${object}/g
 g/PG-KEY/s//${mgikey}/g
 g/PG-TYPE/s//${mgitype}/g
@@ -43,7 +43,7 @@ END
 #
 # execute the script to create the delete function/trigger
 #
-${POSTGRESTRIGGER}/${object}_delete_create.object
+${PG_MGD_DBSCHEMADIR}/trigger/${object}_delete_create.object
 
 done < triggers_delete.txt
 
