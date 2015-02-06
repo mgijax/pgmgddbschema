1f2636ea0590a26ee940caf2dd5d0b220e4dcfe7
Lori Corbani <Lori.Corbani@jax.org>
2015-02-06 15:33:32 +0000

    TR11750/postgres

--- index/index_extra_create.sh
+++ index/index_extra_create.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #
-# cluster all tables
+# see 'exporter/bin/extraIndexes.py'
 #
 
 cd `dirname $0` && . ./Configuration
@@ -9,22 +9,23 @@ cd `dirname $0` && . ./Configuration
 cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0
 
 create index acc_accession_0 on mgd.acc_accession (lower(accID));
+
 create index acc_accession_1 on mgd.acc_accession (lower(prefixPart));
 
 create index acc_mgitype_0 on mgd.acc_mgitype (lower(name));
 
 create index all_label_0 on mgd.all_label (lower(label));
 
-create index mgi_notetype on mgd.mgi_notetype (lower(noteType));
+create index mgi_notetype_0 on mgd.mgi_notetype (lower(noteType));
 
-create index mgi_synonymtype on mgd.mgi_synonymtype (lower(synonymType));
+create index mgi_synonymtype_0 on mgd.mgi_synonymtype (lower(synonymType));
 
-create index mrk_label on mgd.mrk_label (lower(label));
+create index mrk_label_0 on mgd.mrk_label (lower(label));
 
-create index mrk_lcoation_cache on mgd.mrk_lcoation_cache (lower(chromosome));
+create index mrk_lcoation_cache_0 on mgd.mrk_lcoation_cache (lower(chromosome));
 
-create index voc_annottype on mgd.voc_annottype (lower(name));
+create index voc_annottype_0 on mgd.voc_annottype (lower(name));
 
-create index voc_term on mgd.voc_term (lower(term));
+create index voc_term_0 on mgd.voc_term (lower(term));
 
 EOSQL
