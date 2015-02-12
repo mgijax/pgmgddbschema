34b910e47de52dcf6b53682838c94d96f3842fb0
MGI Admin <mgiadmin@jax.org>
2015-02-12 18:13:03 +0000

    TR11750/postgres

--- procedure/procedure_drop.sh
+++ procedure/procedure_drop.sh
@@ -10,44 +10,8 @@
 
 cd `dirname $0` && . ./Configuration
 
-cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0
+for i in *drop.object
+do
+$i $*
+done
 
-DROP FUNCTION ACCRef_insert(int,int);
-DROP FUNCTION ACCRef_process(int,int,varchar,int,varchar,int,int);
-DROP FUNCTION ACC_assignJ(int,int);
-DROP FUNCTION ACC_assignMGI(int,varchar,varchar,int);
-DROP FUNCTION ACC_DELETE_byAccKey(int,int);
-DROP FUNCTION ACC_insertNoChecks(int,varchar,int,varchar,int,int,int);
-DROP FUNCTION ACC_setMax(int,varchar);
-DROP FUNCTION ACC_split(varchar);
-DROP FUNCTION ACC_update(int,varchar,int,int);
-DROP FUNCTION ALL_createWildType(int,varchar);
-DROP FUNCTION ALL_insertAllele(int,varchar,varchar,int,int,int,int,int,timestamp,int,varchar,varchar,varchar,varchar,varchar,varchar,varchar,varchar);
-DROP FUNCTION ALL_mergeAllele();
-DROP FUNCTION BIB_getCopyright(int);
-DROP FUNCTION GXD_removeBadGelBand(int);
-DROP FUNCTION MGI_checkUserRole(varchar,varchar);
-DROP FUNCTION MGI_insertReferenceAssoc(int,int,int,varchar);
-DROP FUNCTION MGI_insertSynonym(int,int,int,text,int);
-DROP FUNCTION MGI_resetAgeMinMax(varchar,int);
-DROP FUNCTION MGI_resetSequenceNum(varchar,int);
-DROP FUNCTION MRK_alleleWithdrawal(int,int,int,int,int);
-DROP FUNCTION MRK_deleteWithdrawal(int,int,int);
-DROP FUNCTION MRK_insertHistory(int,int,int,int,int,varchar,timestamp,int,int,timestamp,timestamp);
-DROP FUNCTION MRK_mergeWithdrawal(int,int,int,int,int,int);
-DROP FUNCTION MRK_reloadLocation(int);
-DROP FUNCTION MRK_simpleWithdrawal(int,int,int,varchar,text,int);
-DROP FUNCTION MRK_splitWithdrawal(int,int,int,text);
-DROP FUNCTION MRK_updateOffset(int,int);
-DROP FUNCTION NOM_transferToMGD(int,int);
-DROP FUNCTION NOM_updateReserved(varchar);
-DROP FUNCTION PRB_ageMinMax(varchar);
-DROP FUNCTION PRB_insertReference(int,int);
-DROP FUNCTION PRB_setStrainReview(int,int);
-DROP FUNCTION SEQ_loadMarkerCache(int);
-DROP FUNCTION SEQ_loadProbeCache(int);
-DROP FUNCTION VOC_mergeAnnotations(int,int,int);
-DROP FUNCTION VOC_mergeTerms(int,int);
-DROP FUNCTION VOC_processAnnotHeaderAll(int);
-
-EOSQL
