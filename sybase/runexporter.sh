30e6047102559026fafc99ed9308ce1e9decb7d2
MGI Admin <mgiadmin@jax.org>
2015-02-23 13:00:54 +0000

    TR11750/postgres

--- sybase/runexporter.sh
+++ sybase/runexporter.sh
@@ -58,13 +58,12 @@ i=`basename $i _create.object`
 echo 'bcp out the files from sybase...', ${i} | tee -a ${LOG}
 echo $i | tee -a ${LOG}
 ${MGI_DBUTILS}/bin/bcpout.csh ${MGD_DBSERVER} ${MGD_DBNAME} $i ${EXPORTDATA} $i.bcp | tee -a ${LOG}.${i}.bcp
-#${MGI_DBUTILS}/bin/bcpout.csh ${MGD_DBSERVER} ${MGD_DBNAME} $i ${EXPORTDATA} $i.bcp | tee -a ${LOG}.${i}.bcp &
 done
 fi
 # wait until all jobs invoked above have terminated
 #wait
-echo 'done: bcp out the files from sybase...' | tee -a ${LOG}
-date | tee -a ${LOG}
+#echo 'done: bcp out the files from sybase...' | tee -a ${LOG}
+#date | tee -a ${LOG}
 #
 # end: bcp out the sybase data
 #
