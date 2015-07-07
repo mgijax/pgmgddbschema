1938f067099fcbf658cb5d08c9775e3977ee4178
Lori Corbani <Lori.Corbani@jax.org>
2015-07-07 12:01:08 +0000

    TR11750/postgres

--- objectCounter.sh
+++ objectCounter.sh
@@ -13,6 +13,10 @@ cd ${TOP}/table
 echo "\nTables          `ls *_create.object | wc -l` scripts"
 psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_catalog.pg_tables where schemaname = 'mgd'"
 
+cd ${TOP}/key
+echo "\nPrimary Keys         `ls *_create.object | wc -l` scripts  (`grep -i 'ADD PRIMARY KEY' *_create.object | wc -l` primary keys)"
+#psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_stat_user_indexes where schemaname = 'mgd' and indexrelname like '%_idx%'" 
+
 cd ${TOP}/index
 echo "\nIndexes         `ls *_create.object | wc -l` scripts  (`grep -i '^create .*index ' *_create.object | wc -l` indexes)"
 psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_stat_user_indexes where schemaname = 'mgd' and indexrelname like '%_idx%'" 
