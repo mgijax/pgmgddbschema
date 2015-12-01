e9f040432c4a28abfdbe4f16d6088464a81161ee
Lori Corbani <Lori.Corbani@jax.org>
2015-12-01 17:50:39 +0000

    TR12070/comments

--- comments/comments.sh
+++ comments/comments.sh
@@ -2,11 +2,6 @@
 
 cd `dirname $0` && . ../Configuration
 
-#
-# comments
-#
-echo "updating comments..." | tee -a ${LOG}
 ./comments.py
 chmod 775 ${PG_MGD_DBSCHEMADIR}/comments/*object
-#psql -U ${PG_DBUSER} -d ${PG_DBNAME} < comments.txt
 
