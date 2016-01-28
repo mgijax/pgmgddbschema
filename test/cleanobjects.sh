9fde2f7ad01972ac8a45e951570f11be50aa645c
Lori Corbani <Lori.Corbani@jax.org>
2016-01-28 18:30:18 +0000

    TR12223/gxd anatomy II

--- test/cleanobjects.sh
+++ test/cleanobjects.sh
@@ -12,28 +12,60 @@ cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
 
 CREATE TEMP TABLE toDelete
 as select a.* 
-from acc_accession a
-where a._mgitype_key = 9
-and not exists (select 1 from img_image s where a._object_key = s._image_key)
+from mgi_note a
+where a._mgitype_key = 3
+and not exists (select 1 from prb_probe s where a._object_key = s._probe_key)
 ;
 
-CREATE INDEX toDelete_idx1 ON toDelete(_Annot_key);
+CREATE INDEX toDelete_idx1 ON toDelete(_Note_key);
 
 select * from toDelete;
 
-delete FROM ACC_Accession
-using toDelete
-WHERE toDelete._Accession_key = ACC_Accession._Accession_key
+--delete FROM mgi_note
+--using toDelete
+--WHERE toDelete._probe_key = mgi_note._probe_key
+--;
+
+select count(a.*)
+from mgi_note a
+where a._mgitype_key = 3
+and not exists (select 1 from prb_probe s where a._object_key = s._probe_key)
 ;
 
-select count(*)
-from acc_accession a
+select count(a.*)
+from mgi_note a
+where a._mgitype_key = 3
+and exists (select 1 from prb_probe s where a._object_key = s._probe_key)
+;
+
+EOSQL
+
+cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
+
+CREATE TEMP TABLE toDelete
+as select a.* 
+from mgi_note a
 where a._mgitype_key = 9
 and not exists (select 1 from img_image s where a._object_key = s._image_key)
 ;
 
-select count(*)
-from acc_accession a
+CREATE INDEX toDelete_idx1 ON toDelete(_Note_key);
+
+select * from toDelete;
+
+--delete FROM mgi_note
+--using toDelete
+--WHERE toDelete._note_key = mgi_note._note_key
+--;
+
+select count(a.*)
+from mgi_note a
+where a._mgitype_key = 9
+and not exists (select 1 from img_image s where a._object_key = s._image_key)
+;
+
+select count(a.*)
+from mgi_note a
 where a._mgitype_key = 9
 and exists (select 1 from img_image s where a._object_key = s._image_key)
 ;
@@ -45,29 +77,93 @@ cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
 CREATE TEMP TABLE toDelete
 as select a.* 
 from mgi_note a
-where a._mgitype_key = 3
-and not exists (select 1 from prb_probe g where a._object_key = s._probe_key)
+where a._mgitype_key = 10
+and not exists (select 1 from prb_strain s where a._object_key = s._strain_key)
 ;
 
-CREATE INDEX toDelete_idx1 ON toDelete(_Annot_key);
+CREATE INDEX toDelete_idx1 ON toDelete(_Note_key);
 
 select * from toDelete;
 
-delete FROM mgi_note
-using toDelete
-WHERE toDelete._probe_key = mgi_note._probe_key
+--delete FROM mgi_note
+--using toDelete
+--WHERE toDelete._note_key = mgi_note._note_key
+--;
+
+select count(a.*)
+from mgi_note a
+where a._mgitype_key = 10
+and not exists (select 1 from prb_strain s where a._object_key = s._strain_key)
 ;
 
 select count(a.*)
 from mgi_note a
-where a._mgitype_key = 3
-and not exists (select 1 from prb_probe g where a._object_key = s._probe_key)
+where a._mgitype_key = 10
+and exists (select 1 from prb_strain s where a._object_key = s._strain_key)
+;
+
+EOSQL
+
+cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
+
+CREATE TEMP TABLE toDelete
+as select a.* 
+from mgi_note a
+where a._mgitype_key = 11
+and not exists (select 1 from all_allele s where a._object_key = s._allele_key)
+;
+
+CREATE INDEX toDelete_idx1 ON toDelete(_Note_key);
+
+select * from toDelete;
+
+--delete FROM mgi_note
+--using toDelete
+--WHERE toDelete._note_key = mgi_note._note_key
+--;
+
+select count(a.*)
+from mgi_note a
+where a._mgitype_key = 11
+and not exists (select 1 from all_allele s where a._object_key = s._allele_key)
 ;
 
 select count(a.*)
 from mgi_note a
-where a._mgitype_key = 3
-and exists (select 1 from prb_probe g where a._object_key = s._probe_key)
+where a._mgitype_key = 11
+and exists (select 1 from all_allele s where a._object_key = s._allele_key)
+;
+
+EOSQL
+
+cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a $LOG
+
+CREATE TEMP TABLE toDelete
+as select a.* 
+from mgi_note a
+where a._mgitype_key = 12
+and not exists (select 1 from gxd_genotype s where a._object_key = s._genotype_key)
+;
+
+CREATE INDEX toDelete_idx1 ON toDelete(_Note_key);
+
+select * from toDelete;
+
+--delete FROM mgi_note
+--using toDelete
+--WHERE toDelete._note_key = mgi_note._note_key
+--;
+
+select count(a.*)
+from mgi_note a
+where a._mgitype_key = 12
+and not exists (select 1 from gxd_genotype s where a._object_key = s._genotype_key)
+;
+
+select count(a.*)
+from mgi_note a
+where a._mgitype_key = 12
+and exists (select 1 from gxd_genotype s where a._object_key = s._genotype_key)
 ;
 
 EOSQL
