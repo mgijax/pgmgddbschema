941174b39d6158f43f8cf2eab4a530f842b9ac93
Lori Corbani <Lori.Corbani@jax.org>
2016-01-29 15:47:45 +0000

    TR12223/gxd anatomy II

--- test/objectmgitype.sh
+++ test/objectmgitype.sh
@@ -14,121 +14,121 @@ select _mgitype_key, name from acc_mgitype order by _mgitype_key;
 
 select distinct _mgitype_key from acc_accession order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 1
 and not exists (select 1 from bib_refs s where a._object_key = s._refs_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 2
 and not exists (select 1 from mrk_marker s where a._object_key = s._marker_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 3
 and not exists (select 1 from prb_probe s where a._object_key = s._probe_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 4
 and not exists (select 1 from mld_expts s where a._object_key = s._expt_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 5
 and not exists (select 1 from prb_source s where a._object_key = s._source_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 6
 and not exists (select 1 from gxd_antibody s where a._object_key = s._antibody_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 7
 and not exists (select 1 from gxd_antigen s where a._object_key = s._antigen_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 8
 and not exists (select 1 from gxd_assay s where a._object_key = s._assay_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 9
 and not exists (select 1 from img_image s where a._object_key = s._image_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 10
 and not exists (select 1 from prb_strain s where a._object_key = s._strain_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
-where _mgitype_key = 11
+where a._mgitype_key = 11
 and not exists (select 1 from all_allele s where a._object_key = s._allele_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
-where _mgitype_key = 12
+where a._mgitype_key = 12
 and not exists (select 1 from gxd_genotype s where a._object_key = s._genotype_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
-where _mgitype_key = 13
+where a._mgitype_key = 13
 and not exists (select 1 from voc_term s where a._object_key = s._term_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 19
 and not exists (select 1 from seq_sequence s where a._object_key = s._sequence_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 20
 and not exists (select 1 from mgi_organism s where a._object_key = s._organism_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 21
 and not exists (select 1 from nom_marker s where a._object_key = s._nomen_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 25
 and not exists (select 1 from voc_evidence s where a._object_key = s._annotevidence_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 28
 and not exists (select 1 from all_cellline s where a._object_key = s._cellline_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 35
 and not exists (select 1 from img_imagepane s where a._object_key = s._ImagePane_key)
 ;
 
-select count(a.*)
+select a.*
 from acc_accession a
 where a._mgitype_key = 39
 and not exists (select 1 from mrk_cluster s where a._object_key = s._cluster_key)
@@ -138,15 +138,15 @@ and not exists (select 1 from mrk_cluster s where a._object_key = s._cluster_key
 
 select distinct _mgitype_key from img_imagepane_assoc order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from img_imagepane_assoc a
-where _mgitype_key = 11
+where a._mgitype_key = 11
 and not exists (select 1 from all_allele s where a._object_key = s._allele_key)
 ;
 
-select count(a.*)
+select a.*
 from img_imagepane_assoc a
-where _mgitype_key = 12
+where a._mgitype_key = 12
 and not exists (select 1 from gxd_genotype s where a._object_key = s._genotype_key)
 ;
 
@@ -154,13 +154,13 @@ and not exists (select 1 from gxd_genotype s where a._object_key = s._genotype_k
 
 select distinct _mgitype_key from map_coord_feature order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from map_coord_feature a
 where a._mgitype_key = 2
 and not exists (select 1 from mrk_marker s where a._object_key = s._marker_key)
 ;
 
-select count(a.*)
+select a.*
 from map_coord_feature a
 where a._mgitype_key = 19
 and not exists (select 1 from seq_sequence s where a._object_key = s._sequence_key)
@@ -170,97 +170,96 @@ and not exists (select 1 from seq_sequence s where a._object_key = s._sequence_k
 
 select distinct _mgitype_key from map_coordinate order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from map_coordinate a
 where a._mgitype_key = 27
 and not exists (select 1 from mrk_chromosome s where a._object_key = s._chromosome_key)
 ;
 
 -- mgi_note
--- delete from mgi_notetype where _notetype_key = 1006
 
 select distinct _mgitype_key from mgi_notetype order by _mgitype_key;
 select distinct _mgitype_key from mgi_note order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 1
 and not exists (select 1 from bib_refs s where a._object_key = s._refs_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 2
 and not exists (select 1 from mrk_marker s where a._object_key = s._marker_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 3
 and not exists (select 1 from prb_probe s where a._object_key = s._probe_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 9
 and not exists (select 1 from img_image s where a._object_key = s._image_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 10
 and not exists (select 1 from prb_strain s where a._object_key = s._strain_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 11
 and not exists (select 1 from all_allele s where a._object_key = s._allele_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 12
 and not exists (select 1 from gxd_genotype s where a._object_key = s._genotype_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 13
 and not exists (select 1 from voc_term s where a._object_key = s._term_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 19
 and not exists (select 1 from seq_sequence s where a._object_key = s._sequence_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 21
 and not exists (select 1 from nom_marker s where a._object_key = s._nomen_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 25
 and not exists (select 1 from voc_evidence s where a._object_key = s._annotevidence_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 36
 and not exists (select 1 from all_cellline_derivation s where a._object_key = s._derivation_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 40
 and not exists (select 1 from mgi_relationship s where a._object_key = s._relationship_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_note a
 where a._mgitype_key = 41
 and not exists (select 1 from voc_evidence_property s where a._object_key = s._evidenceproperty_key)
@@ -270,7 +269,7 @@ and not exists (select 1 from voc_evidence_property s where a._object_key = s._e
 
 select distinct _mgitype_key from mgi_property order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from mgi_property a
 where a._mgitype_key = 39
 and not exists (select 1 from mrk_cluster s where a._object_key = s._cluster_key)
@@ -280,9 +279,9 @@ and not exists (select 1 from mrk_cluster s where a._object_key = s._cluster_key
 
 select distinct _mgitype_key from mgi_reference_assoc order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from mgi_reference_assoc a
-where _mgitype_key = 11
+where a._mgitype_key = 11
 and not exists (select 1 from all_allele s where a._object_key = s._allele_key)
 ;
 
@@ -290,7 +289,7 @@ and not exists (select 1 from all_allele s where a._object_key = s._allele_key)
 
 select distinct _mgitype_key from mgi_set order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from mgi_setmember a, mgi_set aa
 where aa._mgitype_key = 13
 and a._set_key = aa._set_key
@@ -301,84 +300,83 @@ and not exists (select 1 from voc_term s where a._object_key = s._term_key)
 
 select distinct _mgitype_key from mgi_synonym order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from mgi_synonym a
 where a._mgitype_key = 2
 and not exists (select 1 from mrk_marker s where a._object_key = s._marker_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_synonym a
 where a._mgitype_key = 10
 and not exists (select 1 from prb_strain s where a._object_key = s._strain_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_synonym a
 where a._mgitype_key = 11
 and not exists (select 1 from all_allele s where a._object_key = s._allele_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_synonym a
 where a._mgitype_key = 13
 and not exists (select 1 from voc_term s where a._object_key = s._term_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_synonym a
 where a._mgitype_key = 21
 and not exists (select 1 from nom_marker s where a._object_key = s._nomen_key)
 ;
 
-
 -- mgi_translation
 
 select distinct _mgitype_key from mgi_translationtype order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from mgi_translation a, mgi_translationtype aa
 where aa._mgitype_key = 5
 and a._TranslationType_key = aa._TranslationType_key
 and not exists (select 1 from prb_source s where a._object_key = s._source_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_translation a, mgi_translationtype aa
 where aa._mgitype_key = 10
 and a._TranslationType_key = aa._TranslationType_key
 and not exists (select 1 from prb_strain s where a._object_key = s._strain_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_translation a, mgi_translationtype aa
 where aa._mgitype_key = 13
 and a._TranslationType_key = aa._TranslationType_key
 and not exists (select 1 from voc_term s where a._object_key = s._term_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_translation a, mgi_translationtype aa
 where aa._mgitype_key = 20
 and a._TranslationType_key = aa._TranslationType_key
 and not exists (select 1 from mgi_organism s where a._object_key = s._organism_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_translation a, mgi_translationtype aa
 where aa._mgitype_key = 24
 and a._TranslationType_key = aa._TranslationType_key
 and not exists (select 1 from prb_tissue s where a._object_key = s._tissue_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_translation a, mgi_translationtype aa
 where aa._mgitype_key = 28
 and a._TranslationType_key = aa._TranslationType_key
 and not exists (select 1 from all_cellline s where a._object_key = s._cellline_key)
 ;
 
-select count(a.*)
+select a.*
 from mgi_translation a, mgi_translationtype aa
 where aa._mgitype_key = 36
 and a._TranslationType_key = aa._TranslationType_key
@@ -387,7 +385,7 @@ and not exists (select 1 from all_cellline_derivation s where a._object_key = s.
 
 -- voc_annotheader
 
-select count(a.*)
+select a.*
 from voc_annotheader a
 where a._annottype_key in (1002)
 and not exists (select 1 from gxd_genotype s where a._object_key = s._genotype_key)
@@ -397,19 +395,19 @@ and not exists (select 1 from gxd_genotype s where a._object_key = s._genotype_k
 
 select distinct _mgitype_key from voc_annottype order by _mgitype_key;
 
-select count(a.*)
+select a.*
 from voc_annot a
 where a._annottype_key in (1002, 1005)
 and not exists (select 1 from gxd_genotype s where a._object_key = s._genotype_key)
 ;
 
-select count(a.*)
+select a.*
 from voc_annot a
 where a._annottype_key in (1000, 1003, 1006, 1007, 1010, 1011, 1015, 1016, 1017)
 and not exists (select 1 from mrk_marker s where a._object_key = s._marker_key)
 ;
 
-select count(a.*)
+select a.*
 from voc_annot a
 where a._annottype_key in (1012, 1014)
 and not exists (select 1 from all_allele s where a._object_key = s._allele_key)
