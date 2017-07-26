32900fb5211028cea0577d55d4a51ae3e5408d81
Lori Corbani <lori.corbani@jax.org>
2017-07-26 07:33:27 -0400

    TR12250/Lit Triage

--- objectCounter.sh
+++ objectCounter.sh
@@ -14,10 +14,6 @@ cd ${TOP}/table
 echo "Tables          `ls *_create.object | wc -l` scripts"
 psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_catalog.pg_tables where schemaname = 'mgd'"
 
-cd ${TOP}/key
-echo "Primary Keys         `ls *_create.object | wc -l` scripts  (`grep -i 'ADD PRIMARY KEY' *_create.object | wc -l` primary keys)"
-psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_stat_user_indexes where schemaname = 'mgd' and indexrelname like '%_pkey'" 
-
 cd ${TOP}/index
 echo "Indexes         `ls *_create.object | wc -l` scripts  (`grep -i '^create' *_create.object | wc -l` indexes)"
 psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_stat_user_indexes where schemaname = 'mgd' and indexrelname not like '%_pkey'" 
@@ -35,11 +31,13 @@ echo "Views           `ls *_create.object | wc -l` scripts"
 psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_catalog.pg_views where schemaname = 'mgd'" 
 
 cd ${TOP}/key
-echo "Primary Keys            `ls *_create.object | wc -l` scripts  (`grep -i 'PRIMARY KEY' *_create.object | wc -l` foreign keys)"
-psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(c.conname) from pg_constraint c join pg_namespace n on n.oid = c.connamespace where c.contype in ('p') and n.nspname = 'mgd'"
+echo "Primary Keys         `ls *_create.object | wc -l` scripts  (`grep -i 'ADD PRIMARY KEY' *_create.object | wc -l` primary keys)"
+psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(*) from pg_stat_user_indexes where schemaname = 'mgd' and indexrelname like '%_pkey'" 
 
-echo "Keys            `ls *_create.object | wc -l` scripts  (`grep -i '^ALTER TABLE' *_create.object | grep -i 'FOREIGN KEY' | wc -l` foreign keys)"
+cd ${TOP}/key
+echo "Foreign Keys            `ls *_create.object | wc -l` scripts  (`grep -i '^ALTER TABLE' *_create.object | grep -i 'FOREIGN KEY' | wc -l` foreign keys)"
 psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select count(c.conname) from pg_constraint c join pg_namespace n on n.oid = c.connamespace where c.contype in ('f') and n.nspname = 'mgd'"
 
+echo "Duplicate Primary/Foreign Keys"
 psql -h ${PG_DBSERVER} -U ${PG_DBUSER} -d ${PG_DBNAME} --command "select c.conname as duplidateKeys from pg_constraint c join pg_namespace n on n.oid = c.connamespace where c.contype in ('f') and n.nspname = 'mgd' and c.conname like '%fkey1%'"
 
