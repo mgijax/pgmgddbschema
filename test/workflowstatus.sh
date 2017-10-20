1c8639d7543df501ec8a6732fe51e9c5fd1dc85e
Lori Corbani <lori.corbani@jax.org>
2017-10-20 12:17:14 -0400

    TR12250/Lit Triage

--- test/workflowstatus.sh
+++ test/workflowstatus.sh
@@ -5,6 +5,10 @@
 #
 # verify that bib_workflow_status makes sense
 #
+# by Group
+# Indexed
+# Full-coded
+#
 
 cd `dirname $0` && . ./Configuration
 
@@ -18,6 +22,10 @@ cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee $0.log
 -- AP
 --
 
+--
+-- Indexed
+-- MP annot = no
+--
 select distinct 'AP/Indexed', r._Refs_key, r.jnumID, g.term, t.term
 from BIB_Citation_Cache r, BIB_Workflow_Status s, VOC_Term g, VOC_Term t
 where exists (select 1 from MGI_Reference_Assoc gi where gi._Refs_key = r._Refs_key and gi._MGIType_key = 11)
@@ -33,6 +41,10 @@ and s._Status_key != 31576673
 and s.isCurrent = 1
 ;
 
+--
+-- Full-coded
+-- MP annot = yes
+-- 
 select distinct 'AP/Full-coded', r._Refs_key, r.jnumID, g.term, t.term
 from BIB_Citation_Cache r, BIB_Workflow_Status s, VOC_Term g, VOC_Term t
 where exists (select 1 from VOC_Evidence e, VOC_Annot a
@@ -51,6 +63,11 @@ and s.isCurrent = 1
 -- GXD
 --
 
+--
+-- Indexed
+-- GXD_Index = yes
+-- GXD_Assay = no
+--
 select distinct 'GXD/Indexed', r._Refs_key, r.jnumID, g.term, t.term
 from BIB_Citation_Cache r, BIB_Workflow_Status s, VOC_Term g, VOC_Term t
 where exists (select 1 from GXD_Index gi where gi._Refs_key = r._Refs_key)
@@ -63,6 +80,10 @@ and s._Status_key != 31576673
 and s.isCurrent = 1
 ;
 
+--
+-- Full-coded
+-- GXD_Assay = yes
+--
 select distinct 'GXD/Full-coded', r._Refs_key, r.jnumID, g.term, t.term
 from BIB_Citation_Cache r, BIB_Workflow_Status s, VOC_Term g, VOC_Term t
 where exists (select 1 from GXD_Assay ga where ga._Refs_key = r._Refs_key)
@@ -78,14 +99,16 @@ and s.isCurrent = 1
 -- GO
 --
 
+-- 
+-- Indexed
+-- GO Annot = no
+-- Marker = yes
+-- not Indexed
+-- not Rejected : due to original migration using "selcted (bib_dataset)" 
+--
 select distinct 'GO/Indexed', r._Refs_key, r.jnumID, g.term, t.term
 from BIB_Citation_Cache r, BIB_Workflow_Status s, VOC_Term g, VOC_Term t
-where exists (select 1 from BIB_DataSet_Assoc dbsa
-	where dbsa._DataSet_key in (1005)
-	and dbsa._Refs_key = r._Refs_key
-	and dbsa.isNeverUsed = 0
-	)
-and not exists (select 1 from VOC_Evidence e, VOC_Annot a
+where not exists (select 1 from VOC_Evidence e, VOC_Annot a
         where e._Refs_key = r._Refs_key
         and e._Annot_key = a._Annot_key
         and a._AnnotType_key = 1000)
@@ -94,11 +117,15 @@ and r._Refs_key = s._Refs_key
 and s._Group_key = g._Term_key
 and s._Status_key = t._Term_key
 and s._Group_key = 31576666
-and s._Status_key != 31576673
+and s._Status_key not in (31576673, 31576672)
 and s.isCurrent = 1
 ;
 
 
+--
+-- Full-coded
+-- GO Annot = yes
+--
 select distinct 'GO/Full-coded', r._Refs_key, r.jnumID, g.term, t.term
 from BIB_Citation_Cache r, BIB_Workflow_Status s, VOC_Term g, VOC_Term t
 where exists (select 1 from VOC_Evidence e, VOC_Annot a
@@ -117,6 +144,11 @@ and s.isCurrent = 1
 -- QTL
 --
 
+--
+-- Indexed
+-- Mapping = no
+-- QTL Marker = yes
+--
 select distinct 'QTL/Indexed', r._Refs_key, r.jnumID, g.term, t.term
 from BIB_Citation_Cache r, BIB_Workflow_Status s, VOC_Term g, VOC_Term t
 where not exists (select 1 from MLD_Expts gi where gi._Refs_key = r._Refs_key
@@ -133,6 +165,11 @@ and s._Status_key != 31576673
 and s.isCurrent = 1
 ;
 
+--
+-- Full-coded
+-- Mapping = yes
+-- QTL Marker = yes
+--
 select distinct 'QTL/Full-coded', r._Refs_key, r.jnumID, g.term, t.term
 from BIB_Citation_Cache r, BIB_Workflow_Status s, VOC_Term g, VOC_Term t
 where exists (select 1 from MLD_Expts gi where gi._Refs_key = r._Refs_key
